a379bcb7309f63a7df8e3fb1dffed5db8040b000
Niranjana <niranjanalearner@gmail.com>
2023-02-09 12:40:15 +0530

    KOTHAI DISCUSSION

---
+++ Kothdiscussion/src/Begintolearn/declaredouble.java
@@ -0,0 +1,35 @@
+package Begintolearn;
+
+public class declaredouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int int_a=100;
+int int_b=200;
+float float_c=300.1f;
+float float_d=400.1f;
+double double_e=500.14;
+double double_f=600.6;
+String String_1="Nisha";
+String String_2="Kothai";
+
+System.out.println(int_a);
+System.out.println(int_b);
+System.out.println(int_a+int_b);
+
+System.out.println(float_c);
+System.out.println(float_d);
+System.out.println(float_c+float_d);
+
+System.out.println(double_e);
+System.out.println(double_f);
+System.out.println(double_e+double_f);
+
+System.out.println(String_1);
+System.out.println(String_2);
+System.out.println(String_1+String_2);
+
+System.out.println(int_a+int_b+float_c+float_d+double_e+double_f+String_1+String_2);
+	}
+
+}
